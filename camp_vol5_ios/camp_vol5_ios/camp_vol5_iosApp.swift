69b2b548553b9f79d16eebd931caf513e06656ef
ruirui14 <2230103@ecc.ac.jp>
2025-06-19 18:00:13 +0900

    first commit

---
+++ camp_vol5_ios/camp_vol5_ios/camp_vol5_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  camp_vol5_iosApp.swift
+//  camp_vol5_ios
+//
+//  Created by rui on 2025/06/19.
+//
+
+import SwiftUI
+
+@main
+struct camp_vol5_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ camp_vol5_watch/camp_vol5_watch Watch App/camp_vol5_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  camp_vol5_watchApp.swift
+//  camp_vol5_watch Watch App
+//
+//  Created by rui on 2025/06/19.
+//
+
+import SwiftUI
+
+@main
+struct camp_vol5_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
