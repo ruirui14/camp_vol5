6b1b2ec505f58fc4d3c0435325f9b45799c902e8
ruirui14 <2230103@ecc.ac.jp>
2025-06-21 18:23:06 +0900

    詳細画面ほぼできた

---
+++ camp_vol5_ios/camp_vol5_ios/detail_Card.swift
@@ -0,0 +1,18 @@
+//
+//  detail_Card.swift
+//  camp_vol5_ios
+//
+//  Created by rui on 2025/06/21.
+//
+
+import SwiftUI
+
+struct detail_Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    detail_Card()
+}
