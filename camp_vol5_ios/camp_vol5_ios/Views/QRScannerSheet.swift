40f3aaff72d9eaa2fe83fb63c25ec76bfc6fec8e
Fuuma <masa81hiro69@gmail.com>
2025-10-08 19:54:21 +0900

    refactor

--- camp_vol5_ios/camp_vol5_ios/Views/QRScannerSheet.swift
+++ camp_vol5_ios/camp_vol5_ios/Views/QRScannerSheet.swift
@@ -1,4 +1,7 @@
 // Views/QRScannerSheet.swift
+// QRコードスキャナーのシートビュー
+// QRScannerViewControllerをSwiftUIでラップして表示
+
 import AVFoundation
 import SwiftUI
 
@@ -124,133 +127,6 @@ struct QRScannerViewController_Wrapper: UIViewControllerRepresentable {
 
         func didScanQRCode(_ code: String) {
             onQRCodeScanned(code)
-        }
-    }
-}
-
-protocol QRScannerDelegate: AnyObject {
-    func didScanQRCode(_ code: String)
-}
-
-class QRScannerViewController: UIViewController {
-    weak var delegate: QRScannerDelegate?
-
-    private var captureSession: AVCaptureSession!
-    private var previewLayer: AVCaptureVideoPreviewLayer!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupCamera()
-        setupUI()
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        if !captureSession.isRunning {
-            captureSession.startRunning()
-        }
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        if captureSession.isRunning {
-            captureSession.stopRunning()
-        }
-    }
-
-    private func setupCamera() {
-        captureSession = AVCaptureSession()
-
-        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video)
-        else {
-            return
-        }
-
-        let videoInput: AVCaptureDeviceInput
-
-        do {
-            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-        } catch {
-            return
-        }
-
-        if captureSession.canAddInput(videoInput) {
-            captureSession.addInput(videoInput)
-        } else {
-            return
-        }
-
-        let metadataOutput = AVCaptureMetadataOutput()
-
-        if captureSession.canAddOutput(metadataOutput) {
-            captureSession.addOutput(metadataOutput)
-
-            metadataOutput.setMetadataObjectsDelegate(
-                self,
-                queue: DispatchQueue.main
-            )
-            metadataOutput.metadataObjectTypes = [.qr]
-        } else {
-            return
-        }
-
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.frame = view.layer.bounds
-        previewLayer.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-    }
-
-    private func setupUI() {
-        // オーバーレイビューを追加
-        let overlayView = UIView(frame: view.bounds)
-        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        view.addSubview(overlayView)
-
-        // スキャンエリアを作成
-        let scanArea = CGRect(
-            x: 50,
-            y: 200,
-            width: view.bounds.width - 100,
-            height: view.bounds.width - 100
-        )
-        let path = UIBezierPath(rect: overlayView.bounds)
-        let scanPath = UIBezierPath(rect: scanArea)
-        path.append(scanPath.reversing())
-
-        let maskLayer = CAShapeLayer()
-        maskLayer.path = path.cgPath
-        maskLayer.fillRule = .evenOdd
-        overlayView.layer.mask = maskLayer
-
-        // ガイドラインを追加
-        let guidelineView = UIView(frame: scanArea)
-        guidelineView.layer.borderColor = UIColor.white.cgColor
-        guidelineView.layer.borderWidth = 2
-        guidelineView.layer.cornerRadius = 8
-        view.addSubview(guidelineView)
-    }
-}
-
-extension QRScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
-    func metadataOutput(
-        _: AVCaptureMetadataOutput,
-        didOutput metadataObjects: [AVMetadataObject],
-        from _: AVCaptureConnection
-    ) {
-        if let metadataObject = metadataObjects.first {
-            guard
-                let readableObject = metadataObject
-                    as? AVMetadataMachineReadableCodeObject
-            else {
-                return
-            }
-            guard let stringValue = readableObject.stringValue else { return }
-
-            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-
-            delegate?.didScanQRCode(stringValue)
         }
     }
 }
