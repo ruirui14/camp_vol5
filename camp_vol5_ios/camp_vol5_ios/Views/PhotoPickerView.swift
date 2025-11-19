c2ae354e216df3117d5e991ab354c4b41cf6e4c9
Fuuma <masa81hiro69@gmail.com>
2025-11-20 00:02:59 +0900

    feat: 場所移動

--- camp_vol5_ios/camp_vol5_ios/Views/PhotoPickerView.swift
+++ camp_vol5_ios/camp_vol5_ios/Views/PhotoPickerView.swift
@@ -5,52 +5,52 @@
 //  シンプルな写真選択ビュー
 //
 
-import PhotosUI
-import SwiftUI
+// import PhotosUI
+// import SwiftUI
 
-struct SimplePhotoPickerView: UIViewControllerRepresentable {
-    @Binding var selectedImage: UIImage?
-    let onImageSelected: () -> Void
-    @Environment(\.presentationMode) var presentationMode
+// struct SimplePhotoPickerView: UIViewControllerRepresentable {
+//     @Binding var selectedImage: UIImage?
+//     let onImageSelected: () -> Void
+//     @Environment(\.presentationMode) var presentationMode
 
-    func makeUIViewController(context: Context) -> PHPickerViewController {
-        var configuration = PHPickerConfiguration()
-        configuration.filter = .images
-        configuration.selectionLimit = 1
+//     func makeUIViewController(context: Context) -> PHPickerViewController {
+//         var configuration = PHPickerConfiguration()
+//         configuration.filter = .images
+//         configuration.selectionLimit = 1
 
-        let picker = PHPickerViewController(configuration: configuration)
-        picker.delegate = context.coordinator
-        return picker
-    }
+//         let picker = PHPickerViewController(configuration: configuration)
+//         picker.delegate = context.coordinator
+//         return picker
+//     }
 
-    func updateUIViewController(_: PHPickerViewController, context _: Context) {}
+//     func updateUIViewController(_: PHPickerViewController, context _: Context) {}
 
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
+//     func makeCoordinator() -> Coordinator {
+//         Coordinator(self)
+//     }
 
-    class Coordinator: NSObject, PHPickerViewControllerDelegate {
-        var parent: SimplePhotoPickerView
+//     class Coordinator: NSObject, PHPickerViewControllerDelegate {
+//         var parent: SimplePhotoPickerView
 
-        init(_ parent: SimplePhotoPickerView) {
-            self.parent = parent
-        }
+//         init(_ parent: SimplePhotoPickerView) {
+//             self.parent = parent
+//         }
 
-        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-            picker.dismiss(animated: true)
+//         func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+//             picker.dismiss(animated: true)
 
-            guard let result = results.first else {
-                return
-            }
+//             guard let result = results.first else {
+//                 return
+//             }
 
-            result.itemProvider.loadObject(ofClass: UIImage.self) { image, _ in
-                if let image = image as? UIImage {
-                    DispatchQueue.main.async {
-                        self.parent.selectedImage = image
-                        self.parent.onImageSelected()
-                    }
-                }
-            }
-        }
-    }
-}
+//             result.itemProvider.loadObject(ofClass: UIImage.self) { image, _ in
+//                 if let image = image as? UIImage {
+//                     DispatchQueue.main.async {
+//                         self.parent.selectedImage = image
+//                         self.parent.onImageSelected()
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
